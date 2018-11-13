24d1899795484d65a9e977ae6cc0ef9d1e67e3ef
Dalyah <dalyah_se@outlook.com>
2018-11-13 12:00:58 -0700

    Modify according to PEP-8 Guidelines

--- media.py
+++ media.py
@@ -1,6 +1,14 @@
 # Movie Class
 class Movie:
+    """Movie class containes the attributes of a single movie.
+
+    Attributes:
+        title (String): the title of the movie.
+        poster_img_url (String): the URL of the movie poster.
+        trailer_youtube_url (String): the URL of the movie trailer on YouTube.
+        
+    """
     def __init__(self, title, poster_image_url, trailer_youtube_url):
-        self.title=title
-        self.poster_image_url=poster_image_url
-        self.trailer_youtube_url=trailer_youtube_url
+        self.title = title
+        self.poster_image_url = poster_image_url
+        self.trailer_youtube_url = trailer_youtube_url
